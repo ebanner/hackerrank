cf85ddb06199ee0e5210ca41682cc4a554821c65
Edward Banner <edward.banner@gmail.com>
2026-04-02 17:13:34 -0400

    🟢

--- Apple and Orange/main.clj
+++ Apple and Orange/main.clj
@@ -22,22 +22,20 @@
   (for [x oranges] (+ x b)))
 
 (defn countApplesAndOranges [s t a b apples oranges]
-  (let [num-apples
-        (reduce* [num-apples 0
-                  apple-destination (get-apple-destinations apples a)]
+  (def num-apples
+    (reduce* [num-apples 0
+              apple-destination (get-apple-destinations apples a)]
 
-          (if (≤ s apple-destination t)
-            (inc num-apples)
-            num-apples))
+             (if (≤ s apple-destination t)
+               (inc num-apples)
+               num-apples)))
 
-        num-oranges
-        (reduce* [num-oranges 0
-                  orange-destination (get-orange-destinations oranges b)]
+  (def num-oranges
+    (reduce* [num-oranges 0
+              orange-destination (get-orange-destinations oranges b)]
 
-          (if (≤ s orange-destination t)
-            (inc num-oranges)
-            num-oranges))]
+             (if (≤ s orange-destination t)
+               (inc num-oranges)
+               num-oranges)))
 
-    [num-apples num-oranges]))
-
-(countApplesAndOranges 7 11 5 15 [-2 2 1] [5 -6])
+  [num-apples num-oranges])
