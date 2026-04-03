0913df7dc00e0036038297c5a3fd476a50b9d035
Edward Banner <edward.banner@gmail.com>
2026-04-02 20:49:14 -0400

    🟡 The Grid Search

---
+++ The Grid Search/main.clj
@@ -0,0 +1,43 @@
+;
+; Complete the 'gridSearch' function below.
+;
+; The function is expected to return a STRING.
+; The function accepts following parameters:
+;  1. STRING_ARRAY G
+;  2. STRING_ARRAY P
+;
+
+(defmacro all? [[binding coll] & body]
+  `(every? (fn [~binding] ~@body) ~coll))
+
+(defn safe-get-in [coll [i j]]
+  (when (and (< i (count coll))
+             (< j (count (first coll))))
+
+    (get-in coll [i j])))
+
+(defn matches? [G P [row col]]
+  (let [n (count P)
+        m (count (first P))]
+
+    (every? identity
+            (for [i (range n)
+                  j (range m)]
+
+              (= (safe-get-in G [(+ row i) (+ col j)])
+                 (safe-get-in P [i j]))))))
+
+(defn gridSearch [G-strings P-strings]
+  (let [G (mapv vec G-strings)
+        P (mapv vec P-strings)
+        N (count G)
+        M (count (first G))]
+
+    (->
+     (some identity
+       (for [i (range N)
+             j (range M)]
+
+         (matches? G P [i j])))
+
+     (if "YES" "NO"))))
