7eb50df1cc6d31ef0ba19b6ed1d13e37a1156d1d
Edward Banner <edward.banner@gmail.com>
2026-04-03 22:07:02 -0400

    🟢 Subarray Division

--- Subarray Division/main.clj
+++ Subarray Division/main.clj
@@ -8,13 +8,14 @@
 ;  3. INTEGER m
 ;
 
-(defn ++ [a b]
-  (+ (if a 1 0) (if b 1 0)))
+(defn ⊕ [a b]
+  (+ a (if b 1 0)))
 
 (defn birthday [S d m]
   (def N (count S))
 
-  (reduce ++
-   (for [i (range (- N m 1))]
-     (let [window (subvec S i (+ i m))]
-       (= (reduce + window) d)))))
+  (reduce ⊕ 0
+    (for [i (range (inc (- N m)))]
+      (let [window (subvec S i (+ i m))]
+
+        (= (reduce + window) d)))))
--- Subarray Division/test.clj
+++ Subarray Division/test.clj
@@ -2,8 +2,16 @@
 
 (load-file "main.clj")
 
-(deftest T
+(deftest t1
   (is (= (birthday [1 2 1 3 2] 3 2)
          2)))
+
+(deftest t2
+  (is (= (birthday [1 1 1 1 1 1] 3 2)
+         0)))
+
+(deftest t3
+  (is (= (birthday [4] 4 1)
+         1)))
 
 (run-tests)
